6d5b94ade3e5a7e0904f7053fa6901a77c7edf00
RoxanaDC <cretu2roxana@gmail.com>
2025-03-10 02:39:28 +0100

    changed colors grey-yellow

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.remove('light-mode');
+  document.body.classList.add('dark-mode');
+} else {
+  document.body.classList.remove('dark-mode');
+  document.body.classList.add('light-mode');
+}
+
 const projects = $('.projects');
 
 $.ajax('./json/projects.json').then((data) => {
