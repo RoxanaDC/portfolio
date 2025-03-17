4942a37f8bfdc578b3c293397df5af4d23fe2e71
RoxanaDC <cretu2roxana@gmail.com>
2025-03-17 14:44:39 +0100

    particles + bordeaux bg color

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 /* 1. TREBUIE PUS IN ----------- APP.JS ------- CARE SE AFLA IN ------- ALL PAGES HTML (INDEX(HOME) - DEV_WEB - GRAPHISTE)---------THEMELE DARK / LIGHT------------ */
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.classList.remove('light-mode');
-  document.body.classList.add('dark-mode');
-} else {
-  document.body.classList.remove('dark-mode');
-  document.body.classList.add('light-mode');
-}
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+  }
+});
