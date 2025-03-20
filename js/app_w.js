30cb9d89171ca9d629d02b388940336416ea5671
RoxanaDC <cretu2roxana@gmail.com>
2025-03-20 04:17:00 +0100

    remaking spaces for larger fonts

--- js/app_w.js
+++ js/app_w.js
@@ -9,27 +9,29 @@ $.ajax('./json/projects.json').then((data) => {
       <div class="group">
         <div class="projet-details center-link-project">
           
-          <div class="text">
-            Lors de la soutenance de ce projet, j'ai reçu l'évaluation suivante:<br><br> 
+          <div class="text evaluation_com"><h3>
+            Lors de la soutenance de ce projet, j'ai reçu l'évaluation suivante:</h3> 
             "${project.evaluation}"
           </div>
-          <div class="evaluator_code_ahref">
-              <a href=${project.sitedeployed} target="_blank">
-                <img src="./images/${project.image}" alt="${project.title}" loading="lazy" width="300" height="200" /><br><br>
-              </a>
-
-
-            <p>Evalué par:
+          <div class="container_pt_evaluator_image" >
+            <div class="evaluator_code_ahref"><br>
+                        <p>Evalué par:
               <a href="${project.evaluatorpage}" target="_blank">${project.evaluatorname}</a>
             </p>
-            <p>Evaluateur OpenClassrooms</p>
-            <p>---</p>
-            <a href="${project.link}" target="_blank">
-              Découvrez ${project.details} de ce projet sur ${project.hostedon}
-            </a>
+            <p>Evaluateur OpenClassrooms</p><br>
+
+
+              <a href=${project.sitedeployed} target="_blank">
+
+                <img src="./images/${project.image}" alt="${project.title}" loading="lazy" />
+              </a>
+             </div>
+
           </div>
         </div>
       </div>
+                  
+
     `);
 
     projects.append(div);
